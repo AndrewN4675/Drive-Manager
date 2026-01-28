b892e8be6fdb8d3bb177becd6378053acfeb1d3e
AndrewN4675 <andrewcneal03@gmail.com>
2026-01-28 14:28:58 -0800

    added old MAUI pages

---
+++ Old MAUI pages/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Drive_Manager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 1050;
+            const int newHeight = 550;
+           
+            window.MaximumWidth = newWidth + 100;
+            window.MaximumHeight = newHeight + 100;
+            window.MinimumWidth = newWidth;
+            window.MinimumHeight = newHeight;
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
+    }
+}
